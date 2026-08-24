e7dd6ea13b86e83beeb5dfbed84c06a4b5f9d6bd
pertuzjonatha49-droid <pertuzjonatha49@gmail.com>
2026-08-24 07:49:38 -0500

    Add files via upload

---
+++ ejercicio_4.py
@@ -0,0 +1,13 @@
+# Definimos dos edades
+edad1 = 22
+edad2 = 17
+
+# Obtenemos los tres booleanos solicitados
+son_iguales = edad1 == edad2
+primera_mayor = edad1 > edad2
+ambas_mayores_18 = edad1 > 18 and edad2 > 18
+
+# Imprimimos los resultados
+print("¿Son iguales?", son_iguales)
+print("¿La primera es mayor?", primera_mayor)
+print("¿Ambas son mayores de 18?", ambas_mayores_18)
