9be274b6725ca87af7645d91072faaea060a40fd
pertuzjonatha49-droid <pertuzjonatha49@gmail.com>
2026-08-24 07:49:12 -0500

    Add files via upload

---
+++ ejercicio_3.py
@@ -0,0 +1,8 @@
+# Definimos un número a evaluar
+numero = 7
+
+# Usamos el operador módulo % para verificar si es par
+if numero % 2 == 0:
+    print(f"El número {numero} es PAR (resto 0)")
+else:
+    print(f"El número {numero} es IMPAR (resto distinto de 0)")
