7a2cd83981aa8d580f694bc1f032350d45d6d582
pertuzjonatha49-droid <pertuzjonatha49@gmail.com>
2026-08-24 07:48:46 -0500

    Add files via upload

---
+++ ejercicio_2.py
@@ -0,0 +1,9 @@
+# Definimos variables con nombre y apellido
+nombre = "Ana"
+apellido = "Gómez"
+
+# Concatenamos usando el operador +, agregamos un espacio
+nombre_completo = nombre + " " + apellido
+
+# Imprimimos el resultado
+print("Nombre completo:", nombre_completo)
